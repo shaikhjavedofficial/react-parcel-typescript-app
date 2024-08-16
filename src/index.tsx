683129957bc1a1f822a98908eea7002094c28443
shaikhjavedofficial <skjaved633@gmail.com>
2024-08-16 08:09:09 +0530

    "Updated dependencies, removed reportWebVitals and setupTests files, and modified imports in test files"

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
 import '@fontsource/roboto/400.css';
 import { SnackbarProvider } from 'notistack';
@@ -43,7 +42,3 @@ if (module.hot) {
   });
 }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
